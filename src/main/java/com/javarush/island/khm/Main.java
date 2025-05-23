5c3458c6c9aec60a7dcf49885c9840c6d5576174
akhmelev <akhmelev@gmail.com>
2025-05-23 19:47:28 +0300

    Fin project!

--- src/main/java/com/javarush/island/khm/Main.java
+++ src/main/java/com/javarush/island/khm/Main.java
@@ -2,6 +2,6 @@ package com.javarush.island.khm;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Complete 100500 Animals");
+        System.out.println("Complete 100500 Animals!");
     }
 }
